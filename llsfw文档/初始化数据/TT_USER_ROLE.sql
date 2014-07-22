e96bf9a89d5e64dd9da569b6f3b2ee04df4497c9
wangkang <wangkangit@163.com>
2014-07-22 15:16:40 +0800

    在h2数据库中date类型与oracle不匹配的处理方式 将date更换为datetime

--- llsfw文档/初始化数据/TT_USER_ROLE.sql
+++
@@ -1,6 +0,0 @@
-﻿SET DEFINE OFF;
-Insert into TT_USER_ROLE
-   (LOGIN_NAME, ROLE_CODE, CREATE_BY, CREATE_DATE)
- Values
-   ('admin', 'admin', 'SYSTEM', TO_DATE('12/15/2013 15:32:41', 'MM/DD/YYYY HH24:MI:SS'));
-COMMIT;
